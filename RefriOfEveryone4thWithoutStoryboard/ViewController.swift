db7d3f702769a944d4ebf0b6a32e18d6bcc2ebc6
simyoju <dawnofspring@daum.net>
2021-08-22 01:08:39 +0900

    Initial Commit

---
+++ RefriOfEveryone4thWithoutStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RefriOfEveryone4thWithoutStoryboard
+//
+//  Created by simyo on 2021/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
