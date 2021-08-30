bdd54102ffbff69c6361fb1519426648169a5913
simyoju <dawnofspring@daum.net>
2021-08-30 14:05:53 +0900

    make cell with collectionView through xib

--- RefriOfEveryone4thWithoutStoryboard/RFTabBarController.swift
+++ RefriOfEveryone4thWithoutStoryboard/RFTabBarController.swift
@@ -11,44 +11,44 @@ class RFTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Home"
         UITabBar.appearance().tintColor = UIColor(named: "MainColor")
+        UITabBar.appearance().barTintColor = .white
         viewControllers = [createHomeNC(), createCommunityNC(), createAddItemNC(), createNoticeNC(), createMyPageNC()]
     }
     
     private func createHomeNC() -> UINavigationController {
         let homeVC = HomeVC()
-        homeVC.title = "home"
-        homeVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
+        homeVC.tabBarItem = UITabBarItem(title: "home", image: UIImage(systemName: "house"), tag: 0)
         return UINavigationController(rootViewController: homeVC)
     }
     
     private func createCommunityNC() -> UINavigationController {
         let communityVC = CommunityVC()
         communityVC.title = "community"
-        communityVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        communityVC.tabBarItem = UITabBarItem(title: "community", image: UIImage(systemName: "network"), tag: 1)
         return UINavigationController(rootViewController: communityVC)
     }
     
     private func createAddItemNC() -> UINavigationController {
         let addItemVC = AddItemVC()
-        addItemVC.title = "addItem"
-        addItemVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 2)
+        addItemVC.tabBarItem = UITabBarItem(title: "addItem",image: UIImage(systemName: "cart.badge.plus"), tag: 2)
         return UINavigationController(rootViewController: addItemVC)
     }
     
     private func createNoticeNC() -> UINavigationController {
         let noticeVC = NoticeVC()
-        noticeVC.title = "notice"
-        noticeVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 3)
+        noticeVC.tabBarItem = UITabBarItem(title: "notice", image: UIImage(systemName: "bell"), tag: 3)
         return UINavigationController(rootViewController: noticeVC)
     }
     
     private func createMyPageNC() -> UINavigationController {
         let myPageVC = MyPageVC()
         myPageVC.title = "myPage"
-        myPageVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 4)
+        myPageVC.tabBarItem = UITabBarItem(title: "myPage", image: UIImage(systemName: "person"), tag: 4)
         return UINavigationController(rootViewController: myPageVC)
     }
     
+
     
 }
--- RefriOfEveryone4thWithoutStoryboard/Screens/HomeVC.swift
+++ RefriOfEveryone4thWithoutStoryboard/Screens/HomeVC.swift
@@ -8,11 +8,54 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    
+    let categories = CategoriesData()
+    let sectionTitles = [" "," "," "]
+    
+    let tableView = UITableView()
+    let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout.init())
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = "home"
+        
+        tableViewLayout()
+        collectionView.setCollectionViewLayout(UICollectionViewLayout.init(), animated: true)
+        tableView.addSubview(collectionView)
+        
+//        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: "categoryCell")
+        
     }
     
+    private func tableViewLayout(){
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
+    
+}
 
+extension HomeVC : UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellWithCollectionView") as! CellWithCollectionView
+        
+        
+        return cell
+    }
 }
