32c70b44c4828d512a9ff8253223e7f106c20814
simyoju <dawnofspring@daum.net>
2021-09-01 15:14:21 +0900

    create tableView and testCell

--- RefriOfEveryone4thWithoutStoryboard/Screens/HomeVC.swift
+++ RefriOfEveryone4thWithoutStoryboard/Screens/HomeVC.swift
@@ -8,11 +8,43 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableView)
+        
+        setting()
 
     }
     
+    func setting(){
+        layout()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func layout(){
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
+    
 
+}
+
+extension HomeVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .yellow
+        return cell
+    }
 }
